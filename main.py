00b0122823af72848af4562075c23bf8ac9b4e43
sstarnovskii <kopernik.t.800@gmail.com>
2019-10-09 17:56:02 +0700

    local: added lines main: formula of annual revenue added

--- main.py
+++ main.py
@@ -2,5 +2,16 @@
 # Developers:   Drachev Nikita (),
 #               Starnovskiy Sergey (),
 #               Zhuravlev Alexander ()
-import local
-social_status = int(input())
+import local as lcl
+social_status = int(input(lcl.STATUS))
+
+# string constants
+
+name_month = [lcl.JAN, lcl.FAB, lcl.MAR, lcl.APR, lcl.MAY, lcl.JUN, lcl.JUL, lcl.AUG, lcl.SEP, lcl.OCT, lcl.NOV, lcl.DEC]
+
+annual_income = 0
+for month in range(12):
+    print('{} {} {}:'.format(lcl.QUESTION,name_month[month], lcl.CURRENCY, end =''))
+    income = float(input())
+    annual_income += income
+print(annual_income)
