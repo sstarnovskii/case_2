a49730997b8843a5903d4475cf7268fe32b7dd16
sstarnovskii <kopernik.t.800@gmail.com>
2019-10-09 18:03:46 +0700

    changed lcl -> lc to shorten some lines

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 # Developers:   Drachev Nikita (),
 #               Starnovskiy Sergey (),
 #               Zhuravlev Alexander ()
-import local as lcl
+import local as lc
 
-social_status = int(input(lcl.STATUS))
+social_status = int(input(lc.STATUS))
 
-name_month = [lcl.JAN, lcl.FAB, lcl.MAR, lcl.APR, lcl.MAY, lcl.JUN, lcl.JUL, lcl.AUG, lcl.SEP, lcl.OCT, lcl.NOV, lcl.DEC]
+name_month = [lc.JAN, lc.FAB, lc.MAR, lc.APR, lc.MAY, lc.JUN, lc.JUL, lc.AUG, lc.SEP, lc.OCT, lc.NOV, lc.DEC]
 
 # Calculating annual income.
 annual_income = 0
 for month in range(12):
-    print('{} {} {}:'.format(lcl.QUESTION, name_month[month], lcl.CURRENCY, end=''))
+    print('{} {} {}:'.format(lc.QUESTION, name_month[month], lc.CURRENCY, end=''))
     income = float(input())
     annual_income += income
 print(annual_income)
