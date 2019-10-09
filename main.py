72144fb38f26e2b0abc5af89e6338b26718bbcb2
sstarnovskii <kopernik.t.800@gmail.com>
2019-10-09 18:02:25 +0700

    comments added

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 #               Starnovskiy Sergey (),
 #               Zhuravlev Alexander ()
 import local as lcl
-social_status = int(input(lcl.STATUS))
 
-# string constants
+social_status = int(input(lcl.STATUS))
 
 name_month = [lcl.JAN, lcl.FAB, lcl.MAR, lcl.APR, lcl.MAY, lcl.JUN, lcl.JUL, lcl.AUG, lcl.SEP, lcl.OCT, lcl.NOV, lcl.DEC]
 
+# Calculating annual income.
 annual_income = 0
 for month in range(12):
-    print('{} {} {}:'.format(lcl.QUESTION,name_month[month], lcl.CURRENCY, end =''))
+    print('{} {} {}:'.format(lcl.QUESTION, name_month[month], lcl.CURRENCY, end=''))
     income = float(input())
     annual_income += income
 print(annual_income)
