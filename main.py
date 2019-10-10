f774eb7ef8910b266b2c14565e0eb0bdf30e3a4e
sstarnovskii <kopernik.t.800@gmail.com>
2019-10-10 19:34:22 +0700

    fixed local.py added few TODO's 2nd soc status calc done

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for month in range(12):
     annual_income += income
 print(annual_income)  # TODO: remove this "print" later
 tax = 0
-# calculating tax value for every stage of revenue
+# Calculating tax value for each social status and annual revenue.
 if social_status == 1:
     if annual_income <= 9075:
         tax = annual_income * 0.1
@@ -32,7 +32,24 @@ if social_status == 1:
         tax = 9075 * 0.1 + 36900 * 0.15 + 89350 * 0.25 + 186350 * 0.28 + 405101 * 0.33 + (annual_income - 405100 * 0.35)
     if 406751 <= annual_income:
         tax = 9075 * 0.1 + 36900 * 0.15 + 89350 * 0.25 + 186350 * 0.28 + 405101 * 0.33 + 406750 * 0.35 + (annual_income - 406750) * 0.396
+elif social_status == 2:
+    if annual_income <= 18150:
+        tax = annual_income * 0.1
+    if 18151 <= annual_income < 73800:
+        tax = 18150 * 0.1 + (annual_income - 18150) * 0.15
+    if 73801 <= annual_income < 148850:
+        tax = 18150 * 0.1 + 73800 * 0.15 + (annual_income - 73800) * 0.25
+    if 148851 <= annual_income < 226850:
+        tax = 18150 * 0.1 + 73800 * 0.15 + 148850 * 0.25 + (annual_income - 148850) * 0.28
+    if 226851 <= annual_income < 405100:
+        tax = 18150 * 0.1 + 73800 * 0.15 + 148850 * 0.25 + 226850 * 0.28 + (annual_income - 226850) * 0.33
+    if 405101 <= annual_income < 457600:
+        tax = 18150 * 0.1 + 73800 * 0.15 + 148850 * 0.25 + 226850 * 0.28 + 405100 * 0.33 + (annual_income - 405100 * 0.35)
+    if 457601 <= annual_income:
+        tax = 18150 * 0.1 + 73800 * 0.15 + 148850 * 0.25 + 226850 * 0.28 + 405100 * 0.33 + 457600 * 0.35 + (annual_income - 457600) * 0.396
+else:
+    print("For Nikita")
+# TODO: copy 1 more time with column 3 stats for "else"
+
+# TODO: make a nice data output (using local.py for phrases)
 print(tax)
-# todo: копировать еще 2 раза с заменой числовых данных
-
-
